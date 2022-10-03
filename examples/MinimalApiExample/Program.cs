4e55b9561699583b93f90acb9d501774edf662bb
Ben Foster <ben@planetcloud.co.uk>
2022-10-03 11:34:15 +0100

    Add examples

---
+++ examples/MinimalApiExample/Program.cs
@@ -0,0 +1,34 @@
+using System.Globalization;
+using Minid;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapPost("/customers", () =>
+{
+    return new
+    {
+        id = Id.NewId("cus")
+    };
+});
+
+app.MapGet("/customers/{id}", (Id id) =>
+{
+    return new
+    {
+        id
+    };
+});
+
+app.MapPost("/customers/{id}/disable", (DisableRequest disableRequest) =>
+{
+    return disableRequest;
+});
+
+app.Run();
+
+
+public class DisableRequest
+{
+    public Id OperatorId { get; set; }
+}
